f7b69dba62f2d1055f494a8b55c7a98b9f143143
TMonies <15614796+TaylorNBurrows@users.noreply.github.com>
2019-12-02 21:34:37 -0800

    final push

--- index.js
+++ index.js
@@ -2,43 +2,58 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
-const genHTML = require("./generateHTML")
+const genHTML = require("./generateHTML");
+const createPDF = require("html-pdf");
+const options = { format: "Letter"}
+
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub username?"
-      },
-      {
-        type: "input",
-        name: "FavColor",
-        message: "What is your favorite color?"
-      },
-      
-    ]);
-  }
-  promptUser()
-  .then(function(data) {
-    const html = generateHTML(data);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "list",
+      message: "What is your favorite color?",
+      name: "FavColor",
+      choices: [
+        "green",
+        "blue",
+        "pink",
+        "red"
+      ]
+    }
+  ]);
+}
 
-    return writeFileAsync("index.html", html);
-  })
-  .then(function() {
-    console.log("Successfully wrote to index.html");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+  function init() {
+    promptUser()
+    .then(function ({ username, FavColor }) {
+      const color = FavColor
+
+      const queryURL = `https://api.github.com/users/${username}`
+      axios.get(queryURL).then(function (res) {
+        console.log(res)
+
+        res.data.color = color
+
+        const html = genHTML(res.data);
+
+        console.log(html)
+        
+        createPDF.create(html, options).toFile('./profile.pdf', function(err, res) {
+          if (err) return console.log(err);
+          console.log(res); // { filename: '/app/businesscard.pdf' }
+        });
 
 
-// function writeToFile(fileName, data) {
- 
-// }
+      })
 
-// function init() {
+    })
+}
 
-// init();
+init();
